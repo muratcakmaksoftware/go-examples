d0aa103a4a45f85a6280c174484ffd6c662760da
Murat Çakmak <muratcakmak5252@gmail.com>
2025-05-18 22:16:01 +0300

    veritabanı bağlantısı yapıldı

---
+++ go-db-example/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+var db *sql.DB
+
+func main() {
+	godotenv.Load()
+
+	cfg := mysql.NewConfig()
+	cfg.User = os.Getenv("DBUSER")
+	cfg.Passwd = os.Getenv("DBPASS")
+	cfg.Net = "tcp"
+	cfg.Addr = os.Getenv("DBADDR")
+	cfg.DBName = os.Getenv("DBNAME")
+
+	fmt.Println(cfg.FormatDSN()) // root:root@tcp(172.20.0.10:3306)/mydb
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+}

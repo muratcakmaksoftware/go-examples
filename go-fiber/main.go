3fd11d6a839a7a4d9475269bb11471aacc16e042
Murat Çakmak <muratcakmak5252@gmail.com>
2025-05-22 22:13:39 +0300

    route daki bir kaç örnek eksiklikleri tamamlandı

--- go-fiber/main.go
+++ go-fiber/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/utils"
 )
@@ -9,6 +11,19 @@ func main() {
 
 	app := fiber.New(fiber.Config{
 		//Immutable: true, //Tüm requestleri verileri kalıcı durumda olur
+		//Prefork: true, // Bir tane ana process oluşur çekirdek sayısı kadar childs üretir go ilk setup aşamasında
+	})
+
+	/*if fiber.IsChild() == false { //ise ana process demektir child yönetir else ise child process dir child process de ilgili akışı yürütür. Aynı portu dinler
+		fmt.Println("Parent process started")
+	}*/
+
+	app.Use(func(c *fiber.Ctx) error { //tüm requestleri yakalama
+		fmt.Println("Middleware 1")
+		return c.Next() //Diğer handlerları kontrol etmesi için
+	}, func(c *fiber.Ctx) error { //handlers birden fazla tanımlanarak devam edilebiir
+		fmt.Println("Middleware 2")
+		return c.Next()
 	})
 
 	app.Static("/", "./public") // Dizin paylaşımı
@@ -16,6 +31,10 @@ func main() {
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
+	})
+
+	app.Get("/test-error", func(c *fiber.Ctx) error {
+		return fiber.NewError(400, "Custom error message")
 	})
 
 	app.Get("/:murat", func(c *fiber.Ctx) error { //Parametre alimi immutable değil. "Handler içindeysek yapmamız gerekli değil."
@@ -38,6 +57,18 @@ func main() {
 	app.Get("/api/*", func(c *fiber.Ctx) error { //Joker `*` karakteri ile yazılan tüm path bilgisi alınabilir.
 		return c.SendString("API path: " + c.Params("*"))
 	})
+
+	api := app.Group("/v1") // Grup tanımlama
+
+	api.Get("/accounts", func(c *fiber.Ctx) error { // Gruba route tanımlama
+		return c.SendString("accounts")
+	})
+
+	app.Route("/v3", func(api fiber.Router) { //Gruplama
+		api.Get("/product", func(c *fiber.Ctx) error { //Gruba ait route tanımlama
+			return c.SendString("product")
+		}).Name("product") //Route Name Tanımlama
+	}, "products") //Grup name tanımlama
 
 	app.Listen(":3000")
 }
